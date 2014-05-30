56d49930bb86d09fba839d505d2b189d49bd6402
cutsea110 <cutsea110@gmail.com>
2014-05-31 08:42:14 +0900

    init

---
+++ reserve.sh
@@ -0,0 +1,19 @@
+d=`date "+%Y/%m/%d"`
+# 石井先生
+wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=5"
+# 和田先生
+#wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=3"
+# 辻榮先生
+#wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=28"
+# 希望なし
+#wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=1"
+
+
+# 小児科も
+# gHeika1=1
+# 皮膚科も
+# gHeika2=1
+
+# キャンセル
+# mode=1
+
