a4bbd4ba464f7c95891f6faa3101df48e2749eb1
cutsea110 <cutsea110@gmail.com>
2015-09-09 08:01:07 +0900

    update

--- reserve.sh
+++ reserve.sh
@@ -1,6 +1,7 @@
 d=`date "+%Y/%m/%d"`
 # 石井先生
-wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=5"
+wget -O /dev/stdout http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=5" | nkf -w
+#wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=10&min=0&index=0&kaNo=1&koiNo=5"
 # 石井先生の午後
 #wget http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=15&min=0&index=1&kaNo=1&koiNo=5"
 # 和田先生
