feb19e6e3bf0263d6e0133df3ad9fe04e2e3d723
cutsea110 <cutsea110@gmail.com>
2016-02-27 07:47:15 +0900

    make script

---
+++ res2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+koi=20
+
+for idx in "$@";
+do
+  d=`date "+%Y/%m/%d"`
+  h=`expr $idx / 4 \+ 10`
+  m=`expr $idx % 4 \* 15`
+  m=`printf %02d $m`
+  echo wget --inet4-only -O /dev/stdout http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=$h&min=$m&index=$idx&kaNo=3&koiNo=$koi" | nkf -w
+done
+
