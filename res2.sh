db55f09141c8e1d97aea836111d0b6c6438f832b
cutsea110 <cutsea110@gmail.com>
2016-02-27 07:47:52 +0900

    FIX

--- res2.sh
+++ res2.sh
@@ -7,6 +7,6 @@ do
   h=`expr $idx / 4 \+ 10`
   m=`expr $idx % 4 \* 15`
   m=`printf %02d $m`
-  echo wget --inet4-only -O /dev/stdout http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=$h&min=$m&index=$idx&kaNo=3&koiNo=$koi" | nkf -w
+  wget --inet4-only -O /dev/stdout http://ctsrsv.jp/sakura-jibika/rsvroom2.asp --post-data="mode=0&No=100678&date=$d&hour=$h&min=$m&index=$idx&kaNo=3&koiNo=$koi" | nkf -w
 done
 
